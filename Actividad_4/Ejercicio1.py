515794af657c21a6a9a20423bd47550f88106f99
silvadiego-08 <djsilvacubas@uamv.edu.ni>
2025-04-22 11:22:07 -0600

    Ejercicio 1 en Python

---
+++ Actividad_4/Ejercicio1.py
@@ -0,0 +1,9 @@
+inicio = int(input("Ingrese el valor de inicio: "))
+fin = int(input("Ingrese el valor final: "))
+
+numero = int(input("Ingrese un número: "))
+if numero >= inicio and numero <= fin:
+    print("El número es valido.")
+else:
+    print("El número no es valido.")
+   
